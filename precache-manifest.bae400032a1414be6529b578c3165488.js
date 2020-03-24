2bbe9f5079c19f070836a548beefd173f06dd14d
Matias <tinezmatias@gmail.com>
2020-03-24 17:56:37 -0300

    first commit

---
+++ precache-manifest.bae400032a1414be6529b578c3165488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a44be9ceb0299524d25eef8c13b0341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e5b558d7623fde45ea3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "67077b4a18cd0c6ec015",
+    "url": "/static/js/2.58b71e7c.chunk.js"
+  },
+  {
+    "revision": "5e79975ab4e1760dfec791082f238687",
+    "url": "/static/js/2.58b71e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5b558d7623fde45ea3",
+    "url": "/static/js/main.ff0553d8.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
